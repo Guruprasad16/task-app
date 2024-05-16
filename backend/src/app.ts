cfb7952341346c817844ab4fe6b89e1a4ff2a84d
Guruprasad16 <guruprasad1699@gmail.com>
2024-05-16 19:06:26 +0530

    added mongodb, dotenv

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,9 +1,11 @@
 import express from "express";
+import dotenv from "dotenv";
 import appSetup from "./startup/init";
 import routerSetup from "./startup/router";
 import securitySetup from "./startup/security";
 
 const app = express();
+dotenv.config();
 
 appSetup(app);
 routerSetup(app);
--- backend/src/startup/init.ts
+++ backend/src/startup/init.ts
@@ -1,10 +1,17 @@
 import { Express } from "express";
+import mongooseConnect from "../databases/mongodb/mongodb";
 
-const appSetup = (app: Express) => {
-  const APP_PORT = 3000;
-  app.listen(APP_PORT, () => {
-    console.log(`app listening on ${APP_PORT}`);
-  });
+const appSetup = async (app: Express) => {
+  try {
+    await mongooseConnect();
+
+    const APP_PORT = Number(process.env.APP_PORT) || 3000;
+    app.listen(APP_PORT, () => {
+      console.log(`App listening on ${APP_PORT}`);
+    });
+  } catch (error: unknown) {
+    console.log(error);
+  }
 };
 
 export default appSetup;
